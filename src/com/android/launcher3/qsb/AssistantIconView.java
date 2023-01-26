cd52368b1b505f115c913fdc57738f40c7909668
=?UTF-8?q?Ph=E1=BA=A1m=20Ti=C3=AAn=20Sinh?= <song.long.dai.duong@gmail.com>
2023-01-26 13:46:08 +0800

    Launcher3: Optional music search toggle for QSB
    original commit message:
    
    Many users, me included, don't use Google Assistant. Launch Google Music Search instead on pressing Mic icon
    
    mnri: set corresponding icon for lens and music search, minor code refactoring
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I7a520a740ea34bd60a90420a8eb9d36d41011d66
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- src/com/android/launcher3/qsb/AssistantIconView.java
+++ src/com/android/launcher3/qsb/AssistantIconView.java
@@ -7,6 +7,7 @@ import android.util.AttributeSet;
 import android.widget.ImageView;
 import com.android.launcher3.R;
 import com.android.launcher3.qsb.QsbContainerView;
+import com.android.launcher3.Utilities;
 
 public class AssistantIconView extends ImageView {
 
@@ -23,7 +24,13 @@ public class AssistantIconView extends ImageView {
         super(context, attrs, defStyle);
         setScaleType(ScaleType.CENTER);
         setOnClickListener(view -> {
-            Intent intent = new Intent(Intent.ACTION_VOICE_COMMAND).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK).setPackage(QsbContainerView.getSearchWidgetPackageName(context));
+            boolean isMusicSearch = Utilities.isMusicSearchEnabled(context);
+            Intent intent = isMusicSearch ? new Intent(Intent.ACTION_MAIN) : new Intent(Intent.ACTION_VOICE_COMMAND);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setAction(isMusicSearch ? "com.google.android.googlequicksearchbox.MUSIC_SEARCH" : "android.intent.action.VOICE_COMMAND");
+            if (isMusicSearch) {
+                intent.setPackage(QsbContainerView.getSearchWidgetPackageName(context));
+            }
             context.startActivity(intent);
         });
     }
